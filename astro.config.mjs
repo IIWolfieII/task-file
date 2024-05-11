190642ac51b34a070b78d1817f1ebb60abcbd3d0
IIWolfieII <iiwolfieii@yahoo.com>
2024-05-11 22:20:11 +0100

    Added some extensions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
+import robotsTxt from "astro-robots-txt";
+import icon from "astro-icon";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), tailwind(), vue(), robotsTxt(), icon(), sentry(), spotlightjs()]
+});
